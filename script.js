7ffa479ccf8a571f81b54f535a786b4655ea3e55
Bsoirthur <penez.arthur81@gmail.com>
2024-03-20 10:55:56 +0100

    boutons, modal et header

--- script.js
+++ script.js
@@ -26,7 +26,8 @@ fetch('data.json') // requête vers le fichier JSON
         document.getElementById('trackList').appendChild(clone);
     }
 }
-
+ 
+// Templates
   fetch('data.json') // requête vers le fichier JSON  
   .then(response => response.json()) // convertir la réponse textuelle en JSON  
   .then(data => {     // traiter les données   
@@ -42,3 +43,23 @@ fetch('data.json') // requête vers le fichier JSON
  clone.querySelector('.card-img-top').src = data[i].album.images[0].url;        
  clone.querySelector('.card-img-top').alt = data[i].name;          // ajouter le clone au DOM dans le conteneur        
  document.getElementById('trackList').appendChild(clone);     } }
+
+
+ // Modal
+ fetch('data.json')
+ .then(response => response.json())
+ .then(data => {
+   // Une fois les données chargées avec succès, écoutez les clics sur les boutons
+   document.querySelectorAll('.btn').forEach((button, index) => {
+     button.addEventListener('click', function() {
+       // Récupérer les données spécifiques à cette carte
+       const albumName = data[index].album.name;
+       const releaseDate = data[index].album.release_date;
+
+       // Mettre à jour le contenu du modal avec les données récupérées
+       document.getElementById('albumName').textContent = albumName;
+       document.getElementById('releaseDate').textContent = releaseDate;
+     });
+   });
+ })
+ .catch(error => console.error('Erreur lors du chargement du fichier JSON :', error));
